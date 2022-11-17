92c1bb63013fb920696c5338458d0e55e5a188e3
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-17 12:27:17 +0000

    Adding tarfile member sanitization to extractall()

--- test/integration/test_app.py
+++ test/integration/test_app.py
@@ -15,7 +15,26 @@ class TestApp(unittest.TestCase):
         cls.cwd = os.getcwd()
         os.chdir(os.path.dirname(os.path.abspath(__file__)))
         with tarfile.open('data/case1.tar', 'r') as case1:
-            case1.extractall(path='data')
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(case1, path="data")
 
     @classmethod
     def tearDownClass(cls):
